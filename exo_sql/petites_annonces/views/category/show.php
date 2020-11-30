5725f6091216acfb5bc2109e2c78d0b1977cd08b
Pauline <pauline.frare-pradal@laposte.net>
2020-11-30 17:56:39 +0100

    Réorganisation de la pagination

--- exo_sql/petites_annonces/views/category/show.php
+++ exo_sql/petites_annonces/views/category/show.php
@@ -1,17 +1,14 @@
 <?php
 use App\Connection;
 use App\Model\{Post,Category};
-use App\URL;
+use App\PaginatedQuery;
 
 
 $id = (int)$params['id'];
 $slug = $params['slug'];
 
 $pdo = Connection::getPDO();
-$query = $pdo->prepare("
-        SELECT 
-       * FROM category 
-        WHERE id = :id ");
+$query = $pdo->prepare("SELECT * FROM category  WHERE id = :id ");
 $query->execute(['id'=>$id]);
 $query->setFetchMode(PDO::FETCH_CLASS, Category::class);
 /** @var Category | false*/
@@ -22,55 +19,23 @@ if($category === false){
 }
 
 if($category->getSlug() !== $slug){
-    $url = $router->url('category', ['slug'=> $category->getSlug(), 'id' => $id]);
+    $url = $router->url('category', ['slug'=> $category->getSlug(), 'id'=>$category->getID()]);
     //si l'url n'est pas bon
     http_response_code(301);
     header('Location:' . $url);
 }
-/**
- * Paramètres qui varient:
- *
- * $sqlListing: string
- * $classMapping: string
- * $sqlCount: string
- * $pdo: PDO = Connection::getPDO()
- * $perPage: int = 12
- *
- * Methode dont j'ai besoin (qui permet de récuperer les résultats comme les articles pour le listing:
- *
- * getIthems(): array
- * previousPageLink(): ?string
- * nextPageLink(): ?string
- *
- */
 
+$paginatedQuery = new \App\PaginatedQuery(
+"SELECT p.*
+            FROM post p
+            JOIN post_category pc ON pc.post_id = p.id
+            WHERE pc.category_id = {$category->getID()}
+            ORDER BY created_at DESC",
+       "SELECT COUNT(category_id) FROM post_category WHERE category_id = {$category->getID()}"
+);
+/** @var Post[]*/
+$posts = $paginatedQuery->getItems(Post::class);
 
-
-
-$currentPage = URL::getPositiveInt('page', 1);
-
-$count= (int)$pdo
-    ->query("SELECT COUNT(category_id) FROM post_category WHERE category_id = " . $category->getID())
-    ->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-
-$pages = ceil($count / $perPage);
-
-if ($currentPage > $pages){
-    throw new Exception('Cette page n\'existe pas');
-}
-
-$offset = $perPage * ($currentPage-1);
-$query = $pdo->query("
-    SELECT p.* 
-    FROM post p 
-    JOIN post_category pc ON pc.post_id = p.id
-    WHERE pc.category_id = {$category->getID()}
-    ORDER BY created_at DESC 
-    LIMIT $perPage OFFSET $offset
-");
-
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 $link = $router->url('category', ['id'=> $category->getID(), 'slug' => $category->getSlug()]);
 
 $title = "Catégorie {$category->getName()}";
@@ -89,16 +54,6 @@ $title = "Catégorie {$category->getName()}";
 </div>
 
 <div class="d-flex justify-content-between my-4">
-
-    <?php if ($currentPage > 1): ?>
-        <?php
-        $l = $link;
-        if ($currentPage > 2) $l = $link . '?page=' . ($currentPage -1);
-        ?>
-        <a href="<?= $l ?>" class="btn btn-primary">&laquo; Page précédente</a>
-    <?php endif;?>
-
-    <?php if ($currentPage < $pages): ?>
-        <a href="<?= $link ?>?page=<?= ($currentPage +1 )?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
-    <?php endif;?>
+    <?= $paginatedQuery->previousLink($link) ?>
+    <?= $paginatedQuery->nextLink($link) ?>
 </div>
--- exo_sql/petites_annonces/views/post/index.php
+++ exo_sql/petites_annonces/views/post/index.php
@@ -4,14 +4,12 @@ use App\Helpers\Text;
 use App\Model\Post;
 use App\Connection;
 use App\URL;
+use App\PaginatedQuery;
 
 
 $title = 'Mon projet';
 
 $pdo = Connection::getPDO();
-
-$currentPage = URL::getPositiveInt('page', 1);
-
 
 /* Pour mettre en place la pagination. Il va falloir calculer les variables suivantes :
 
@@ -20,22 +18,11 @@ $currentPage = URL::getPositiveInt('page', 1);
    - Le nombre de pages (obtenu en divisant le nombre total d'articles par le nombre d'éléments par page).
 
 Il suffit ensuite de jouer avec le paramètre OFFSET afin d'afficher les articles correspondant à une certaine page. */
-
-
-$count= (int)$pdo->query("SELECT COUNT(id) FROM post")->fetch(PDO::FETCH_NUM)[0];
-
-$perPage = 12;
-
-$pages = ceil($count / $perPage);
-
-if ($currentPage > $pages){
-    throw new Exception('Cette page n\'existe pas');
-}
-
-$offset = $perPage * ($currentPage-1);
-$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
-
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+$paginatedQuery = new \App\PaginatedQuery(
+       "SELECT * FROM post ORDER BY created_at DESC ",
+    "SELECT COUNT(id) FROM post");
+$posts = $paginatedQuery->getItems(Post::class);
+$link = $router->url('home');
 ?>
 
 <h1 >Mon projet</h1>
@@ -49,16 +36,6 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 </div>
 
 <div class="d-flex justify-content-between my-4">
-
-    <?php if ($currentPage > 1): ?>
-        <?php
-        $link = $router->url('home');
-        if ($currentPage > 2) $link .= '?page=' . ($currentPage -1);
-        ?>
-        <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page précédente</a>
-    <?php endif;?>
-
-    <?php if ($currentPage < $pages): ?>
-        <a href="<?= $router->url('home') ?>?page=<?= ($currentPage +1 )?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
-    <?php endif;?>
+    <?= $paginatedQuery->previousLink($link) ?>
+    <?= $paginatedQuery->nextLink($link) ?>
 </div>
